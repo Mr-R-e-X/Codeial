d748759a82bb88812f9788c16ba0a31e1eb1204a
Mr-R-e-X <souvikhazra151@gmail.com>
2023-09-04 01:54:22 +0530

    restricted sign-in and sign-up page if the user is logged in and implemented Mongo Store to solve the problem of user auto log out for every time server restarts.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
 
 app.use(express.urlencoded());
 
@@ -37,7 +38,17 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
-    }
+    },
+    store: new MongoStore(
+        {
+        mongoUrl: 'mongodb://127.0.0.1/codeial_development',
+        collection: 'session',
+        autoRemove: 'disabled',
+        },
+        function(err){
+            console.log(err || 'connect mongodb setup ok!!')
+        }
+    ),
 }));
 
 app.use(passport.initialize());
