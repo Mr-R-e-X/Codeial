2d4460c62890e0c627a42ad2eb16481312392977
Mr-R-e-X <souvikhazra151@gmail.com>
2023-09-06 23:08:49 +0530

    Posts are rendered in home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,29 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
-    console.log(req.cookies);
+    // console.log(req.cookies);
     // res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
-    });
+
+    // Post.find({}).then(function(posts){
+    //     return res.render('home', {
+    //         title: 'Codeial |::| Home',
+    //         posts: posts,
+    //     })
+    // }).catch(function(err){
+    //     if(err){
+    //         console.log(`Error loading the Posts :: ${err}`);
+    //         return;
+    //     }
+    // });
+    Post.find({}).populate('user').exec().then(function(posts){
+        return res.render('home', {
+            title: 'Codeial |::| Home',
+            posts: posts,
+        })
+    }).catch(function(err){
+        if(err){
+            console.log(`Error loading the Posts :: ${err}`);
+            return;
+        }
+    });  
 }
