42c62ad7bb269b099fd591715dd73d9c1703ed11
Mr-R-e-X <souvikhazra151@gmail.com>
2023-09-15 01:41:07 +0530

    more functions changed in async await form

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,34 +1,5 @@
 const User = require("../models/user");
 
-module.exports.profile = async function (req, res) {
-  try {
-    let user = await User.findById(req.params.id);
-
-    return res.render("user_profile", {
-      title: "User Profile",
-      profile_user: user,
-    });
-  } catch (err) {
-    console.log(`ERROR ===> Profile ==> ${err}`);
-    return res.redirect("back");
-  }
-};
-
-module.exports.update = function (req, res) {
-  if (req.user.id == req.params.id) {
-    User.findByIdAndUpdate(req.params.id, req.body)
-      .then(function (user) {
-        return res.redirect("back");
-      })
-      .catch(function (err) {
-        console.log(`ERROR ===> Updating Profile ==> ${err}`);
-        return res.redirect("back");
-      });
-  } else {
-    return res.status(401).send("Unauthorized");
-  }
-};
-
 // render the sign up page
 module.exports.signUp = function (req, res) {
   if (req.isAuthenticated()) {
@@ -51,37 +22,40 @@ module.exports.signIn = function (req, res) {
   });
 };
 
-// get the sign up data
-module.exports.create = function (req, res) {
-  if (req.body.password != req.body.confirm_password) {
+module.exports.profile = async function (req, res) {
+  try {
+    let user = await User.findById(req.params.id);
+
+    return res.render("user_profile", {
+      title: "User Profile",
+      profile_user: user,
+    });
+  } catch (err) {
+    console.log(`ERROR ===> Profile ==> ${err}`);
     return res.redirect("back");
   }
+};
 
-  User.findOne({
-    email: req.body.email,
-  })
-    .then(function (user) {
-      if (!user) {
-        User.create(req.body)
-          .then(function (user) {
-            return res.redirect("/users/sign-in");
-          })
-          .catch(function (err) {
-            if (err) {
-              console.log("Error in finding user in signing up");
-              return;
-            }
-          });
-      } else {
-        return res.redirect("back");
-      }
-    })
-    .catch(function (error) {
-      if (error) {
-        console.log("Error in finding user in signing up");
-        return;
-      }
-    });
+// get the sign up data
+module.exports.create = async function (req, res) {
+
+  try {
+    if (req.body.password != req.body.confirm_password) {
+      return res.redirect("back");
+    }
+    let user = await User.findOne({ email: req.body.email });
+    if(!user){
+      await User.create(req.body);
+      return res.redirect("/users/sign-in");
+    }else {
+      return res.redirect("back");
+    }
+  } catch (err) {
+    if (err) {
+      console.log(`ERROR ===> Sign Up ==> ${err}`);
+      return;
+    }
+  }
 };
 
 // using passport for sign in and create a session
@@ -96,4 +70,18 @@ module.exports.destroySession = function (req, res, next) {
     }
     return res.redirect("/");
   });
+};
+// Updating profile details.
+module.exports.update = async function (req, res) {
+  try {
+    if (req.user.id == req.params.id) {
+      await User.findByIdAndUpdate(req.params.id, req.body);
+      return res.redirect("back");
+    } else {
+      return res.status(401).send(`401 ===> User ==> User Not Found`);
+    }
+  } catch (err) {
+    console.log(`ERROR ===> Updating Profile ==> ${err}`);
+    return;
+  }
 };
--- routes/users.js
+++ routes/users.js
@@ -16,13 +16,9 @@ router.post(
   passport.checkAuthentication,
   usersController.update
 );
-
 router.get("/sign-in", usersController.signIn);
 router.get("/sign-up", usersController.signUp);
 router.post("/create", usersController.create);
-// router.post('/create-session', usersController.createSession);
-// router.post('/sign-out', usersController.signOut);
-
 // use passport as a middleware to authenticate
 router.post(
   "/create-session",
