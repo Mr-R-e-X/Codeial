39591d433a57ad61510c2e0e27e89c7d776da104
Mr-R-e-X <souvikhazra151@gmail.com>
2023-09-24 19:03:22 +0530

    file uploading for profile completed.

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,8 @@
 const User = require("../models/user");
+const fs = require('fs');
+const path = require('path');
+
+
 
 // render the sign up page
 module.exports.signUp = function (req, res) {
@@ -77,15 +81,42 @@ module.exports.destroySession = function (req, res, next) {
 };
 // Updating profile details.
 module.exports.update = async function (req, res) {
-  try {
-    if (req.user.id == req.params.id) {
-      await User.findByIdAndUpdate(req.params.id, req.body);
-      return res.redirect("back");
-    } else {
-      return res.status(401).send(`401 ===> User ==> User Not Found`);
+  
+  if(req.user.id == req.params.id){
+
+    try{
+      
+      let user = await User.findById(req.params.id);
+      User.uploadedAvatar(req, res, function(err){
+        if(err){
+          console.log(`Multer Error ===> ${err}`);
+        }
+        user.name = req.body.name;
+        user.email = req.body.email;
+
+        if(req.file){
+
+          if(user.avatar){
+            fs.unlinkSync(path.join(__dirname, '..' , user.avatar));
+          }
+          // this is saving the path of the uploaded file into the avatar field in the user
+          user.avatar = User.avatarPath + '/' + req.file.filename;
+        }
+        user.save();
+        return res.redirect('back');
+      })
+
+    }catch(err){
+
+      console.log(`ERROR ===> Updating Profile ==> ${err}`);
+      return res.redirect('back');
+
     }
-  } catch (err) {
-    console.log(`ERROR ===> Updating Profile ==> ${err}`);
-    return;
+
+  }else{
+
+    req.flash('error', 'Unauthorized');
+    return res.status(401).send(`401 ===> User ==> User Not Found`);    
+  
   }
 };
