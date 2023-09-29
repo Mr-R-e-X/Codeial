fd665e97438a0924a63cb2647c51e8b3e254f32c
Mr-R-e-X <souvikhazra151@gmail.com>
2023-09-30 02:02:00 +0530

    basic style updated still styling is pending

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -12,7 +12,7 @@ module.exports.home = async function (req, res) {
     let user = await User.find({});
 
     return res.render("home", {
-      title: "Codeial |::| Home",
+      title: "< codeial />",
       posts: posts,
       all_users: user,
     });
