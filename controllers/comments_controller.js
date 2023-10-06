a5d972ec3d6a0a0e1c1f67d6bd96f7e115c4d4fa
Mr-R-e-X <souvikhazra151@gmail.com>
2023-10-07 02:33:27 +0530

    Delayed Jobs implemented using KUE package and with help of Redis server

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,7 +1,9 @@
 const Comment = require("../models/comment");
 const Post = require("../models/post");
 const commentsMailer = require('../mailers/comments_mailer');
-
+const queue = require('../config/kue');
+const commentEmailWorker = require('../workers/comment_email_worker');
+const queueMicroTask = require('../workers/comment_email_worker');
 module.exports.create = async function (req, res) {
   try {
     let post = await Post.findById(req.body.post);
@@ -15,7 +17,15 @@ module.exports.create = async function (req, res) {
       post.save();
       console.log(comment);
       comment = await comment.populate('user', 'name email');
-      commentsMailer.newComment(comment);
+      // commentsMailer.newComment(comment);
+      let job = queue.create('emails', comment).save((err)=>{
+        if(err){
+          console.log(`Error in sending to the queue => ${err}`);
+          return;
+        }
+        console.log(`Job Enqueued => ${job.id}`);
+      });
+
       if(req.xhr){
         return res.status(200).json({
           data: {
