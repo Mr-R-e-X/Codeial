27cbec88f1f8cfa2d4083379bb76bd3cc43a300c
Mr-R-e-X <souvikhazra151@gmail.com>
2023-10-21 22:53:55 +0530

    styling improved

---
+++ controllers/friendship_controller.js
@@ -0,0 +1,15 @@
+const Friendship = require("../models/friendship");
+const User = require("../models/user");
+
+module.exports.toggleFriend = async (req, res){
+
+    try {
+        let addFriend;
+        let reqSend = false;
+
+        
+    } catch (error) {
+        
+    }
+
+} 
